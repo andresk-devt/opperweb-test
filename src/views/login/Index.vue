7f496f08ae3fa237806d9af8fc307b3d943994f6
andresk-devt <learningxandres@gmail.com>
2023-04-27 15:27:34 -0500

    Create a function to get the inner width page, and organice the responsive and desktop form

--- src/views/login/Index.vue
+++ src/views/login/Index.vue
@@ -1,9 +1,12 @@
 <script setup>
-import FooterComponent from "./components/FooterComponent.vue"
+import FooterComponent from "./components/FooterComponent.vue";
+import Extend from "@/Extend/onResize";
+
 import { ref } from "vue";
 
 const email = ref("");
 const password = ref("");
+const { screenWidth } = Extend();
 </script>
 
 <script setup></script>
@@ -11,63 +14,92 @@ const password = ref("");
 <template>
   <div class="login">
     <div class="login-content">
+      <div class="brand-container" v-if="screenWidth > 800">
+        <img src="@/assets/images/logo.png" alt="" />
+        <p class="brand-container__text">
+          anime<span class="brand-container__text brand-container__text--pink">yabu.</span>
+        </p>
+      </div>
       <div class="close-button">
-        <img src="@/assets/images/back-icon.png" alt="Icon">
+        <img src="@/assets/images/back-icon.png" alt="Icon" />
       </div>
-      <div class="login-content-header">
-        <img src="@/assets/images/login-form-image.png" alt="" />
-        <div class="login-content-header__information">
-          <h2>ひさしぶり!</h2>
-          <p>¡Hola, te extrañamos!</p>
-        </div>
-      </div>
-      <div class="login-content-form">
-        <div class="input-container">
-          <label for="email" class="input-container__label">Email</label>
-          <div class="input-container-content">
-            <input
-              id="email"
-              class="input-container__input"
-              v-model="email"
-              placeholder="Email"
-            />
+      <div class="login-content-container">
+        <div class="login-content-header">
+          <img src="@/assets/images/login-form-image.png" alt="" />
+          <div class="login-content-header__information">
+            <h2>ひさしぶり!</h2>
+            <p>¡Hola, te extrañamos!</p>
           </div>
         </div>
-        <div class="input-container">
-          <label for="password" class="input-container__label">Password</label>
-          <div class="input-container-content">
-            <input
-              id="password"
-              class="input-container__input"
-              v-model="password"
-              placeholder="Password"
-            />
-            <ion-icon name="eye-outline"></ion-icon>
+        <div class="login-content-form">
+          <div class="input-container">
+            <label for="email" class="input-container__label">Email</label>
+            <div class="input-container-content">
+              <input
+                id="email"
+                class="input-container__input"
+                v-model="email"
+                placeholder="Email"
+              />
+            </div>
           </div>
-          <span class="forgot-password">¿Olvido la contraseña?</span>
+          <div class="input-container">
+            <label for="password" class="input-container__label"
+              >Password</label
+            >
+            <div class="input-container-content">
+              <input
+                id="password"
+                class="input-container__input"
+                v-model="password"
+                placeholder="Password"
+              />
+              <ion-icon name="eye-outline"></ion-icon>
+            </div>
+            <span class="forgot-password">¿Olvido la contraseña?</span>
+          </div>
+          <button class="button-container">Acceder</button>
         </div>
-        <button class="button-container">
-          Acceder
-        </button>
-      </div>
-      <div class="login-content-footer">
-        <FooterComponent />
+        <div class="login-content-footer">
+          <FooterComponent />
+        </div>
       </div>
     </div>
+    <div class="login-content"></div>
   </div>
 </template>
 
 <style scoped>
 .login {
+  display: flex;
   width: 100vw;
   height: 100vh;
-  background: rgb(228, 135, 251);
-  background: linear-gradient(
-    175deg,
-    rgba(228, 135, 251, 1) 0%,
-    var(--pink-color) 6%,
-    rgba(0, 0, 0, 1) 100%
-  );
+}
+.login-content {
+  width: 50%;
+}
+.login-content-container {
+  padding: 30px 20%;
+}
+.login-content:nth-child(1) {
+  background: black;
+}
+.brand-container {
+  display: flex;
+  align-items: center;
+  padding: 25px;
+}
+.brand-container img {
+  padding-right: 15px;
+}
+.brand-container .brand-container__text {
+  font-family: "poppins", sans-serif;
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: var(--white-color);
+}
+.brand-container .brand-container__text--pink {
+  color: var(--pink-color);
 }
 .login-content-header {
   color: var(--white-color);
@@ -128,24 +160,24 @@ const password = ref("");
   position: absolute;
   font-size: 0.875rem;
   color: var(--pink-light-color);
-  cursor: pointer ;
+  cursor: pointer;
 }
 .button-container {
-  color: #3E1149;
+  color: #3e1149;
   background: var(--pink-light-color);
   border: none;
   outline: none;
   width: 100%;
   height: 45px;
   font-size: 1rem;
-  font-family: 'roboto';
+  font-family: "roboto";
   border-radius: 12px;
   margin: 35px auto;
   cursor: pointer;
 }
 .button-container:hover {
   background: var(--pink-light-color);
-  opacity: .8;
+  opacity: 0.8;
 }
 .login-content-footer {
   width: 90%;
@@ -153,5 +185,23 @@ const password = ref("");
 }
 .close-button {
   padding: 25px;
+}
+
+@media (max-width: 800px) {
+  .login-content-container {
+    padding: 30px;
+  }
+  .login-content:nth-child(1) {
+    width: 100%;
+    background: linear-gradient(
+      175deg,
+      rgba(228, 135, 251, 1) 0%,
+      var(--pink-color) 6%,
+      rgba(0, 0, 0, 1) 100%
+    );
+  }
+  .login-content:nth-child(2) {
+    display: none;
+  }
 }
 </style>
