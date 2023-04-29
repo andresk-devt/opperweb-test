1d8d070ab9074baad5d759c7a129007a3b69e30d
andresk-devt <learningxandres@gmail.com>
2023-04-29 16:22:29 -0500

    Create the menu icon button

--- src/views/layout/Index.vue
+++ src/views/layout/Index.vue
@@ -32,6 +32,9 @@ onMounted(async () => {
 
 <template>
   <div class="layout">
+    <div class="menu-icon-content">
+      <ion-icon name="menu-outline"></ion-icon>
+    </div>
     <div class="layout-sidebar">
       <SidebarLayoutVue />
     </div>
@@ -46,7 +49,13 @@ onMounted(async () => {
   display: flex;
   flex-direction: row;
   height: 100vh;
+  position: relative;
 }
+.menu-icon-content ion-icon {
+  color: var(--white-color);
+  font-size: 3rem;
+  cursor: pointer;
+} 
 .layout-sidebar {
   width: 20%;
   background: var(--purple-color);
@@ -60,7 +69,17 @@ onMounted(async () => {
   color: var(--white-color);
   font-family: "poppins", sans-serif;
 }
+.menu-icon-content {
+  display: none;
+}
+
 @media (max-width: 900px) {
+  .menu-icon-content {
+    display: block;
+    right: 0px;
+    padding: 20px;
+    position: absolute;
+  }
   .layout-content {
     width: 100%;
     padding: 50px;
